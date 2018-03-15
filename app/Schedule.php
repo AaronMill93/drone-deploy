49beba3771aa6fb6a7ce9814d39d91997f5fe987
vagrant <vagrant@homestead>
2018-03-15 12:45:40 +0000

    last optimization revision

--- app/Schedule.php
+++ app/Schedule.php
@@ -27,86 +27,36 @@ class Schedule
     /**
      * Segments locations by package weight and creates trips recursively 
      */
-    // public function createTrips($locations, $weightLimit)
-    // {
-    //     $i = 0;
-    //     $trip = new Trip();
-    //     usort($locations, array($this,"compareLocationsByPackageWeight"));
-
-    //     if (empty($locations)) {
-    //         return 1;
-    //     }
-    
-    //     elseif (count($locations) == 1) {
-    //         if(end($locations)["packageWeight"] <= $weightLimit) {
-    //             $trip->addLocationFromArray(array_pop($locations));
-    //         }
-    //         else{
-    //             return 1;
-    //         }
-    //     }
-    //     else {
-    //         do {
-        
-    //             $arrayValue = array_pop($locations);
-    //             $arrayValueWeight = $arrayValue["packageWeight"];
-    //             $trip->addLocationFromArray($arrayValue);
-        
-    //             $i += $arrayValueWeight;
-        
-    //         } while ((($i + end($locations)["packageWeight"]) <= $weightLimit) && !empty($locations));
-    //     }
-    //     $this->addTrip($trip);
-    //     $this->createTrips($locations, $weightLimit);
-    // }
 
     public function createTrips($locations, $weightLimit)
     {
-        if (empty($locations)) {
-            return 1;
+        //find optimal number of trips
+        $totalLocationWeights = 0;
+        foreach($locations as $location){
+            $totalLocationWeights += $location['packageWeight'];
         }
+        $optimalTrips = ceil($totalLocationWeights / $weightLimit);
 
-        else{
-            $trip = NULL;
-            $newTrip = FALSE;
-            //find appropriate trip
-            foreach($this->trips as $sampleTrip){
-               
-                $scheduleLocation = $locations[0];
-                $scheduleLocationPW = $scheduleLocation['packageWeight'];
-                //if we can add this package to the trip without going over
-                $runningWeightTripPW = 0;
-                foreach($sampleTrip->locations as $sampleTripLocation) {
-                    $runningWeightTripPW += $sampleTripLocation->packageWeight;
+        //sort locations from largest to smallest package weight
+        usort($locations, array($this,"compareLocationsByPackageWeight"));
+        
+        //create trips
+        for($i = 0; $i < $optimalTrips; $i++){
+            $trip = new Trip();
+            $tripWeight = 0;
+            foreach($locations as $key => $location){
+                $tripWeight += $location['packageWeight'];
+                if($tripWeight <= $weightLimit) {
+                    $trip->addLocationFromArray($location);
+                    unset($locations[$i]);
                 }
-                if(($scheduleLocationPW + $runningWeightTripPW) <= $weightLimit) {
-                    //use this trip
-                    $trip = $sampleTrip;
-                    //exit loop
-                    break;
+                else{
+                    //subtract from trip weight since it wasn't added
+                    $tripWeight -= $location['packageWeight'];
                 }
             }
-            if(is_null($trip)) {
-                $trip = new Trip();
-                $newTrip = TRUE;
-            }
-                usort($locations, array($this,"compareLocationsByPackageWeight"));
-                $runningWeightSum = 0;
-                foreach($locations as $key => $location) {
-                    $runningWeightSum += $location['packageWeight'];
-                    if($runningWeightSum <= $weightLimit) {
-                        $trip->addLocationFromArray($location);
-                        array_shift($locations);
-                    }
-                    else{
-                        
-                        break;
-                    }
-                }
-                if($newTrip) {
-                $this->addTrip($trip);
-                }
-                $this->createTrips($locations, $weightLimit);
+            //add trip to schedule
+            $this->addTrip($trip);
         }
     }
 }
